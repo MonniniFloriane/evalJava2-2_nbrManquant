2612d3faa0e3bf54322ad3171e1360ae4903383d
floriane <florianemonnini@gmail.com>
2025-02-04 09:53:28 +0100

    essais rater avec boucle for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(nbrManquant());
+    }
+
+    public static int nbrManquant() {
+        int[] tab = {1, 2, 4, 5, 6};
+        int nbrManquant = 0;
+
+        for (int i : tab) {
+            if (tab[i] == tab[i + 1]) {
+                nbrManquant = tab[i + 1];
+            }
+        }
+        return nbrManquant;
     }
 }
