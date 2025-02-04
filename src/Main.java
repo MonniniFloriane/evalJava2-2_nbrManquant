0f5faf19d6cc3babf686220f21e38f86b738e1b6
floriane <florianemonnini@gmail.com>
2025-02-04 11:43:51 +0100

    algo final

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] tab = {1, 2, 4, 5, 6};
+        int[] tab = {2, 3, 1, 5};
         System.out.println("Entrée : " + Arrays.toString(tab));
-
-        int nbrManquant = nbrManquant(tab);
-        System.out.println("Sortie : " + nbrManquant);
+        System.out.println("Sortie : " + nbrManquant(tab));
     }
 
     public static int nbrManquant(int[] tab) {
-        int N = tab.length + 1;
-        int sum = N * (N - 1) / 2;
+        int N      = tab.length + 1;
+        int sum    = N * (N + 1) / 2;
         int sumTab = 0;
 
         for (int i : tab) {
             sumTab += i;
         }
-        return sumTab - sum;
+
+        return sum - sumTab;
     }
 }
